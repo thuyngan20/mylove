36159dc3be0a7097296e0d974d18e063294e52b8
yuu <duchuvo1998@gmail.com>
2022-11-15 00:12:45 +0700

    Fix slide-show, not fix style. so sorry, i'm forgot.

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,51 +1,25 @@
-// bulmaCarousel.attach('#slider', {
-//     slidesToScroll: 1,
-//     slidesToShow: 3,
-//     infinite: true,
-//   });
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("item-slide");
-//   var captionText = document.getElementById("caption-container");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-// }
-
 var slideIndex = 1;
- showSlides(slideIndex);
 function plusSlides(n) {
-   showSlides(slideIndex += n);
- }
+  showSlides(slideIndex += n);
+}
 function currentSlide(n) {
-   showSlides(slideIndex = n);
- }
+  debugger
+  showSlides(slideIndex = n);
+}
 function showSlides(n) {
-   var i;
-   var slides = document.getElementsByClassName("mySlides");
-   var dots = document.getElementsByClassName("dot");
-   if (n > slides.length) {slideIndex = 1}
-   if (n < 1) {slideIndex = slides.length}
-   for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
-   }
-   for (i = 0; i < dots.length; i++) {
-       dots[i].className = dots[i].className.replace(" active", "");
-   }
-   slides[slideIndex-1].style.display = "block";
-   dots[slideIndex-1].className += " active";
- }
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
+
+  dots[slideIndex - 1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+}
+
+document.getElementsByClassName("dot")[0].onclick();
--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -23,13 +23,13 @@
 #   # Report violations without enforcing the policy.
 #   # config.content_security_policy_report_only = true
 # end
-Rails.application.config.content_security_policy do |policy|
-    policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+# Rails.application.config.content_security_policy do |policy|
+#     policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
   
-    if Rails.env.development?
-      policy.script_src :self, :https, :unsafe_eval
-    else
-      policy.script_src :self, :https
-    end
-  end
+#     if Rails.env.development?
+#       policy.script_src :self, :https, :unsafe_eval
+#     else
+#       policy.script_src :self, :https
+#     end
+#   end
   
